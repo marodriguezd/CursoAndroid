e3dcfab779458f968b7c90db49cb17f12c4691f5
Miguel Ángel Rodríguez Dalí <migueadali@gmail.com>
2024-01-16 11:18:45 +0100

    Todo limpio para empezar

--- AndroidStudioProjects/JetpackComposeTutorial/app/src/main/java/com/marodriguezd/jetpackcomposetutorial/MainActivity.kt
+++ AndroidStudioProjects/JetpackComposeTutorial/app/src/main/java/com/marodriguezd/jetpackcomposetutorial/MainActivity.kt
@@ -15,32 +15,6 @@ import com.marodriguezd.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTh
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            JetpackComposeTutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackComposeTutorialTheme {
-        Greeting("Android")
     }
 }
