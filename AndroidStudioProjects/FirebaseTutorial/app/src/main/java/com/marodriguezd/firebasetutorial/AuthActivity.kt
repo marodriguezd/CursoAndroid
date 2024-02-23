b21a68cf48ab3ff026434b7f96ac0771d4ce7091
marodriguezd <migueadali@gmail.com>
2024-02-23 12:20:04 +0100

    Documentación y mejora de comentarios en código.

--- AndroidStudioProjects/FirebaseTutorial/app/src/main/java/com/marodriguezd/firebasetutorial/AuthActivity.kt
+++ AndroidStudioProjects/FirebaseTutorial/app/src/main/java/com/marodriguezd/firebasetutorial/AuthActivity.kt
@@ -4,26 +4,9 @@ import android.content.Context
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -38,45 +21,42 @@ import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 
+
+/**
+ * Pantalla de autenticación que permite a los usuarios registrarse o iniciar sesión con email y contraseña,
+ * así como iniciar sesión con Google.
+ *
+ * @param onNavigateToHome Callback que se ejecuta para navegar a la pantalla de inicio después
+ */
 @Composable
 fun AuthActivity(onNavigateToHome: (String, ProviderType) -> Unit) {
-    val context = LocalContext.current  // Obtiene el contexto actual en un composable
+    val context = LocalContext.current
 
-    // Estados para almacenar el texto ingresado por el usuario
-    var email by remember {
-        mutableStateOf("")
-    }
-    var password by remember {
-        mutableStateOf("")
-    }
-    var showDialog by remember {
-        mutableStateOf(false)
-    }
-    var errorMessage by remember {
-        mutableStateOf("")
-    }
-    var passwordVisibility by remember {
-        mutableStateOf(false)
-    }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var showDialog by remember { mutableStateOf(false) }
+    var errorMessage by remember { mutableStateOf("") }
+    var passwordVisibility by remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),  // Agregar un poco de padding para no pegar los elementos a los bordes
-        horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        // Campo de texto para el email
-        OutlinedTextField(value = email,
+        OutlinedTextField(
+            value = email,
             onValueChange = { email = it },
             label = { Text("Email") },
-            singleLine = true,  // Para que el TextField sea de una sola línea
-            modifier = Modifier.fillMaxWidth()  // Para que ocupe tod0 el ancho disponible
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
         )
 
-        Spacer(modifier = Modifier.height(8.dp))  // Espacio entre los TextFields
+        Spacer(modifier = Modifier.height(8.dp))
 
-        // Campo de texto para la contraseña
-        OutlinedTextField(value = password,
+        OutlinedTextField(
+            value = password,
             onValueChange = { password = it },
             label = { Text("Contraseña") },
             singleLine = true,
@@ -84,20 +64,14 @@ fun AuthActivity(onNavigateToHome: (String, ProviderType) -> Unit) {
             modifier = Modifier.fillMaxWidth()
         )
 
-        Spacer(modifier = Modifier.height(16.dp))  // Espacio entre el TextField y el Button
+        Spacer(modifier = Modifier.height(16.dp))
 
-        // Definir colores específicos
-        val colorBotonRegistrar = Color(0xFF1967D2) // Google azul 700
-        val colorBotonAcceder = Color(0xFF1B3A57) // Firebase azul marino
-
-        // Fila para contener ambos botones (Registrar y Acceder)
         Row(
-            modifier = Modifier.fillMaxWidth(),  // La fila ocupa tod0 el ancho disponible
-            horizontalArrangement = Arrangement.spacedBy(8.dp)  // Espacio entre los botones
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            // Botón para registrar
             Button(
-                onClick = {
+                onClick = {  // *
                     if (email.isNotEmpty() && password.isNotEmpty()) {
                         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
                             .addOnCompleteListener { task ->
@@ -111,28 +85,14 @@ fun AuthActivity(onNavigateToHome: (String, ProviderType) -> Unit) {
                                 }
                             }
                     }
-                }, colors = ButtonDefaults.buttonColors(
-                    containerColor = colorBotonRegistrar  // Usar el color Google azul 700 definido
-                ), modifier = Modifier.weight(1f)  // Cada botón obtiene la mitad del espacio
+                }, colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF1967D2)),
+                modifier = Modifier.weight(1f)
             ) {
                 Text("Registrar")
             }
 
-            // Diálogo de alerta
-            if (showDialog) {
-                AlertDialog(onDismissRequest = { showDialog = false },
-                    title = { Text("Error") },
-                    text = { Text(errorMessage) },
-                    confirmButton = {
-                        Button(onClick = { showDialog = false }) {
-                            Text("Aceptar")
-                        }
-                    })
-            }
-
-            // Botón para acceder
             Button(
-                onClick = {
+                onClick = {  // *
                     if (email.isNotEmpty() && password.isNotEmpty()) {
                         FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
                             .addOnCompleteListener { task ->
@@ -147,18 +107,27 @@ fun AuthActivity(onNavigateToHome: (String, ProviderType) -> Unit) {
                                 }
                             }
                     }
-                }, colors = ButtonDefaults.buttonColors(
-                    containerColor = colorBotonAcceder  // Usar el color Firebase azul marino definido
-                ), modifier = Modifier.weight(1f)  // Cada botón obtiene la mitad del espacio
+                }, colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF1B3A57)),
+                modifier = Modifier.weight(1f)
             ) {
                 Text("Acceder")
             }
         }
-        Spacer(modifier = Modifier.height(16.dp))  // Espacio entre los botones
 
-        // Sección del tema de Google
+        if (showDialog) {
+            AlertDialog(onDismissRequest = { showDialog = false },
+                title = { Text("Error") },
+                text = { Text(errorMessage) },
+                confirmButton = {
+                    Button(onClick = { showDialog = false }) {
+                        Text("Aceptar")
+                    }
+                }
+            )
+        }
 
-        // Configuración de Google SignIn Options
+        Spacer(modifier = Modifier.height(16.dp))
+
         val googleSignInOptions = remember {
             GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(context.getString(R.string.default_web_client_id))
@@ -166,15 +135,13 @@ fun AuthActivity(onNavigateToHome: (String, ProviderType) -> Unit) {
                 .build()
         }
 
-        // Cliente de Google SignIn
         val googleSignInClient = remember {
             GoogleSignIn.getClient(context, googleSignInOptions)
         }
 
-        // ActivityResultLauncher para el flujo de inicio de sesión de Google
         val googleSignInLauncher = rememberLauncherForActivityResult(
             ActivityResultContracts.StartActivityForResult()
-        ) { result ->
+        ) { result ->  // *
             if (result.resultCode == android.app.Activity.RESULT_OK) {
                 val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
                 try {
@@ -187,47 +154,28 @@ fun AuthActivity(onNavigateToHome: (String, ProviderType) -> Unit) {
         }
 
 
-        // Botón de inicio de sesión con Google
         Button(
             onClick = {
-                googleSignInClient.signOut()  // Opcional: cerrar sesión en el cliente de Google antes de iniciar una nueva sesión
+                googleSignInClient.signOut()  // Para desloguear 100% Google
                 val signInIntent = googleSignInClient.signInIntent
                 googleSignInLauncher.launch(signInIntent)
-                /*// TODO: Implementar lógica de inicio de sesión con Google
-                signInWithGoogle { email, _ ->
-                    saveUserData(context, email, ProviderType.GOOGLE.name)  // Pasa el contexto aquí
-                    onNavigateToHome(email, ProviderType.GOOGLE)
-                }*/
-            }, modifier = Modifier
+            },
+            modifier = Modifier
                 .fillMaxWidth()
-                .height(48.dp),  // Altura estándar para botones
-            colors = ButtonDefaults.outlinedButtonColors(
-            // colors = ButtonDefaults.buttonColors(
-                containerColor = Color(0xFFf2f2f2),  // Color de fondo del botón gris casi blanco
-                contentColor = Color.Unspecified  // Color del contenido (texto e icono), no especificado para que tome el color predeterminado
-                // containerColor = Color(0xFF4285F4)  // Color de Google
-            ),
-            border = BorderStroke(1.dp, Color.LightGray)  // Borde gris claro
+                .height(48.dp),
+            colors = ButtonDefaults.outlinedButtonColors(containerColor = Color(0xFFf2f2f2),contentColor = Color.Unspecified),
+            border = BorderStroke(1.dp, Color.LightGray)
         ) {
             Icon(
                 painter = painterResource(id = R.drawable.google),
                 contentDescription = "Google sign-in",
                 modifier = Modifier.size(24.dp),
-                tint = Color.Unspecified  // No aplicar tinte al icono para mantener sus colores originales
+                tint = Color.Unspecified
             )
             Spacer(Modifier.size(ButtonDefaults.IconSpacing))
-            Text(
-                "Google",
-                color = Color.Black  // Color de texto en negro
-            )
+            Text("Google", color = Color.Black)
         }
     }
-}
-
-fun signInWithGoogle(onNavigateToHome: (String, String) -> Unit) {
-    // Aquí debes implementar la lógica de inicio de sesión con Google utilizando Firebase Authentication
-    // Puedes seguir la documentación oficial de Firebase para configurar el inicio de sesión con Google
-    // https://firebase.google.com/docs/auth/android/google-signin
 }
 
 fun firebaseAuthWithGoogle(idToken: String, context: Context, onNavigateToHome: (String, ProviderType) -> Unit) {
